3b75285aed3d277d1a566e8c2a7d2f314e6fa834
MatiasCaamal <matiascaamal@hotmail.com>
2025-02-11 15:31:32 -0600

    Empezando el GridView

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        tabBarTheme: const TabBarTheme(
+          //Cambia el color del indicador de los tabs
+          indicatorColor: Colors.pink,
+        )
       ),
       home: const HomePage(),
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,12 @@
 import 'package:donut_app_8sc/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
+import '../tabs/burger_tab.dart';
+import '../tabs/donut_tab.dart';
+import '../tabs/pancake_tab.dart';
+import '../tabs/pizza_tab.dart';
+import '../tabs/smoothie_tab.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -11,60 +17,82 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-
     List<Widget> myTabs = [
-    //donut tab
-    const MyTab(iconPath: 'lib/icons/donut.png',),
-    //burger tab
-    const MyTab(iconPath: 'lib/icons/burger.png',),
-    //smoothie tab
-    const MyTab(iconPath: 'lib/icons/donut.png',),
-    //pancake tab
-    const MyTab(iconPath: 'lib/icons/pancakes.png',),
-    //pizza tab
-    const MyTab(iconPath: 'lib/icons/pizza.png',)
+      //donut tab
+      const MyTab(
+        iconPath: 'lib/icons/donut.png',
+      ),
+      //burger tab
+      const MyTab(
+        iconPath: 'lib/icons/burger.png',
+      ),
+      //smoothie tab
+      const MyTab(
+        iconPath: 'lib/icons/donut.png',
+      ),
+      //pancake tab
+      const MyTab(
+        iconPath: 'lib/icons/pancakes.png',
+      ),
+      //pizza tab
+      const MyTab(
+        iconPath: 'lib/icons/pizza.png',
+      )
     ];
-
 
     return DefaultTabController(
       length: myTabs.length,
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.transparent, 
-          leading: Icon(Icons.menu, color: Colors.grey[800],),
-          actions: [Padding(
-            padding: const EdgeInsets.only(right: 12.0),
-            child: IconButton(onPressed: (){}, icon: Icon(Icons.person)),
-          )],),
-          body: Column(
-            children: [
-              // Texto "I want to Eat"
-              const Padding(
-                padding: EdgeInsets.only(left: 24.0),
-                child: Row(
-                  children: [
-                  Text('I want to ', style: TextStyle(fontSize: 32),),
-                  Text('Eat', style: TextStyle(
-                    fontSize: 32, 
-                    fontWeight: FontWeight.bold, 
-                    decoration: TextDecoration.underline),)
-                  ],
-                ),
-              ),
-              // Tab Bar
-              TabBar(tabs: myTabs)
-      
-      
-              // Tab bar view
-      
-      
-              // Carrito
-      
-      
-            ],
+          backgroundColor: Colors.transparent,
+          leading: Icon(
+            Icons.menu,
+            color: Colors.grey[800],
           ),
-      
-      
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 12.0),
+              child: IconButton(onPressed: () {}, icon: Icon(Icons.person)),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            // Texto "I want to Eat"
+            const Padding(
+              padding: EdgeInsets.only(left: 24.0),
+              child: Row(
+                children: [
+                  Text(
+                    'I want to ',
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  Text(
+                    'Eat',
+                    style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline),
+                  )
+                ],
+              ),
+            ),
+            // Tab Bar
+            TabBar(tabs: myTabs),
+
+            // Tab bar view
+            Expanded(
+                child: TabBarView(children: [
+              DonutTab(),
+              BurgerTab(),
+              SmoothieTab(),
+              PancakeTab(),
+              PizzaTab()
+            ]))
+
+            // Carrito
+          ],
+        ),
       ),
     );
   }
